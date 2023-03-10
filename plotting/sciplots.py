23ddcdbd512635ebf7afaa0fbcad76e34a699c16
Matthew Feng <mattfeng@mit.edu>
2023-03-10 10:57:12 -0500

    plotting

--- plotting/sciplots.py
+++ plotting/sciplots.py
@@ -2,20 +2,26 @@
 
 import matplotlib.pyplot as plt
 
-class GroupedBarChart():
-    def __init__(self):
-        pass
-
-class AnnotatedLinePlot():
-    def __init__(self, *args, **kwargs):
+class Chart:
+    def __init__(self, title, *args, **kwargs):
+        self.title = title
         self.fig = plt.figure(*args, **kwargs)
         self.ax = self.fig.gca()
+    
+    def show(self):
+        self.fig.show()
+
+class AnnotatedLinePlot(Chart):
+    def __init__(self, title=None, *args, **kwargs):
+        super().__init__(title)
 
     def add_line(self, x, y, stdev=None):
         self.ax.plot(x, y, marker="o")
 
-    def show(self):
-        self.fig.show()
+class GroupedBarChart(Chart):
+    def __init__(self, title, *args, **kwargs):
+        pass
+
 
 if __name__ == "__main__":
     plot = AnnotatedLinePlot(figsize=(9, 6))
